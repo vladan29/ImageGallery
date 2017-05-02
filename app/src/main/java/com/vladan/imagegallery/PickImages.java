afa81c06d2c975fa96b737829831a5e1c2fb0ffe
vladan3629 <vladan.starcevic@gmail.com>
2017-05-02 16:46:54 +0200

    Initial commit

---
+++ app/src/main/java/com/vladan/imagegallery/PickImages.java
@@ -0,0 +1,13 @@
+package com.vladan.imagegallery;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PickImages extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pick_images);
+    }
+}
